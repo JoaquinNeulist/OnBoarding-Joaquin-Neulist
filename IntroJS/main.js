699602da962432f9c007cfbe88fea93b29c82b22
JoaquinNeulist <joaquinneulist@gmail.com>
2024-03-19 16:30:36 -0300

    clase 19/03 y JS Basic exercises

---
+++ IntroJS/main.js
@@ -0,0 +1,132 @@
+console.log("tu")
+
+/* Low difficulty
+const myName = "my name is Joaquin"
+const myLastName = "Neulist"
+let myAge = 19
+const myPet = "my pet is call Mila"
+let petAge = 4
+let fullName = myName +" "+ myLastName
+let presentationText = fullName+" "+myAge+" "+myPet+" "+petAge
+
+
+console.log(myName)
+console.log(myLastName)
+console.log(myAge)
+console.log(myPet)
+console.log(petAge)
+console.log(fullName)
+console.log(presentationText)
+*/
+
+
+
+
+//CLASE DEL 19/03:
+//Datos: string, number, boolean, null, undefined, 
+
+//let, const
+/*
+let apellido = "Neulist"
+console.log(apellido)
+ const nombre = "Joaquin"
+console.log(nombre,apellido)
+console.log( `${nombre} ${apellido}` )
+*/
+
+/*
+console.log( null == 0) // false
+console.log( null >= 0) // true
+console.log( null <= 0) // true
+console.log( null < 0) // false
+console.log( null > 0) // false
+*/
+
+//let number1 = 10
+//let number2 = 20
+
+// console.log(number1 + number2) // 30
+// console.log(number1 * number2) // 200
+//console.log(number1 - number2) // -10
+//console.log(number2 % number1) // 10 (resto)
+//console.log(number2 / number1) // 2 
+//console.log(100 ** 0.5) // 10 (raiz cuadrada ya que potencia a 1/2) 
+//console.log(number1 ** 2) // 100 potenciar al cuadrado  
+/*let number
+console.log(number)
+number = 1 // declaracion
+console.log(number)
+number++
+console.log(number)
+number-- 
+console.log(number)
+//number+=10 // number = number + 10
+//console.log(number)
+console.log(number*2)
+console.log(number)
+*/
+
+//operadores de comparacion
+
+//==, ====, <, >, !=, !==, <=, >=
+
+/*
+console.log( "1" != 0) //distinto valor
+console.log( "0" != 0) // mismo valor
+console.log( "0" !== 0) //distinto dato, aunque mismo valor 
+*/
+
+//operadores logicos
+// ADN && -- OR || (ascii 124)-- NOT !
+/*
+console.log( true && true) //true
+console.log( true && false) //false
+console.log( false && true) //false
+console.log( false && false) //false
+
+console.log( false || true) //true
+console.log( false || true) //true
+console.log( false || true) //true
+console.log( false || false) //false
+
+console.log( !false ) //true
+console.log( !true ) //false
+
+console.log( 0 == false ) // es false
+console.log( "" == false ) // es false
+console.log( "a" == true ) // no es true
+console.log( 1 == true ) // es true
+// (solo aplica a 0 y 1)
+*/
+
+// objeto
+/*
+let nombre = "Joaquin"
+let apellido = "Neulist"
+
+let mentor = {
+    // key || clave = value || valor
+    nombre  : "Joaquin",
+    apellido : "Neulist"
+}
+let mentor2 = {
+    // key || clave = value || valor
+    nombre  : "Guille",
+    apellido : "Cornetti"
+}
+
+let arrayMentores = [ mentor , mentor2 ] 
+console.log(mentor.nombre) //Joaquin
+console.log(arrayMentores[1].nombre) //Eduardo
+console.log(arrayMentores)
+console.table(arrayMentores)
+console.log(mentor)
+console.table(mentor)
+*/
+
+// alert("Soy una alerta")
+// let nombre = prompt("ingrese su nombre")
+// console.log(typeof(nombre))
+
+//let resultado = confirm("estas seguro")
+//console.log(resultadi)
