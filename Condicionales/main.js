41533cf3fad4ffb20a1ea0cfdc95edf3d6846c17
JoaquinNeulist <joaquinneulist@gmail.com>
2024-03-20 12:14:12 -0300

    funciones de js

--- Condicionales/main.js
+++ Condicionales/main.js
@@ -2,20 +2,33 @@
 
 console.log("hola")
 
-let estaLloviendo 
-let tenesGanas 
+/*let estaLloviendo 
 estaLloviendo = prompt("Ingrese si llueve o no")
 
 if (estaLloviendo == "si") {
-    tenesGanas = prompt("Tenes ganas de salir a correr?")
-    if(tenesGanas =="si"){
-        console.log("salgo a correr")
+    console.log("No salgo a correr")
+}else if(estaLloviendo == "no"){
+    let tengoGanas = confirm("tenes ganas de salir a correr?")
+    if(tengoGanas){
+        console.log("Salgo a correr")
     }else{
         console.log("No salgo a correr")
     }
-}else if(estaLloviendo == "no"){
-
-    console.log("Salgo a correr")
 }else{
     console.log("No entiendo")
+}
+*/
+
+let planInet = "a"
+
+switch(planInet){
+    case "bajo":  console.log("Debe pagar $50");
+    break;
+    case "medio":  console.log("Debe pagar $75");
+    break;
+    case "alto":  console.log("Debe pagar $100");
+    break;
+    case "vip":  console.log("Debe pagar $200");
+    break;
+    default:console.log("Plan desconocido");
 }
