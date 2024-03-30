578936ed84d9a93d44766fd0dbbe4d7b05d44a88
JoaquinNeulist <joaquinneulist@gmail.com>
2024-03-30 20:58:34 -0300

    Creacion de archivos de ejercicios de funciones y metodos superior

---
+++ ExercisesBeers/main.js
@@ -0,0 +1,9 @@
+//1:
+let printMessage = message => console.log(message)
+//2:
+let createMultiplication = (num1, num2) => {
+    let res = num1 * num2
+    console.log(res) 
+}
+createMultiplication(2,4)
+//3:
