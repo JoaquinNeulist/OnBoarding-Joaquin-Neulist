7e119b0122bcbdde3ccf805a9525b61e95f71275
JoaquinNeulist <joaquinneulist@gmail.com>
2024-04-08 14:23:16 -0300

    correcion del ejercicios 4

--- EventsExercise/main.js
+++ EventsExercise/main.js
@@ -47,7 +47,7 @@ inputResult.value = IMC.toFixed(2)
 }
 thirdBoton.addEventListener('click', resultadoIMC)
 */
-
+/*
 //4:
 let convertBoton = document.getElementById('convertButton')
 let deleteBoton = document.getElementById('deleteButton')
@@ -57,25 +57,20 @@ let clearInputs = function() {
     inputDolar.value = null;
     inputLocal.value = null;
 }
-let localtodolar = function() {
+let convert = function() {
     let monedaLocal = parseFloat(inputLocal.value);
-    if (isNaN(monedaLocal) || monedaLocal <= 0) {
-        alert("Ingrese valores válidos de moneda local");
-        return;
-    }
-    inputDolar.value = monedaLocal / 500;
-}
-
-let dolartolocal = function() {
     let dolar = parseFloat(inputDolar.value);
-    if (isNaN(dolar) || dolar <= 0) {
-        alert("Ingrese valores válidos de dólares");
+    if ((!monedaLocal || monedaLocal <= 0) && (!dolar || dolar <= 0)) {
+        alert("Ingrese un valor válido para la moneda local o los dólares");
         return;
     }
-    inputLocal.value = dolar * 500;
-}
-
-
-convertBoton.addEventListener('click', localtodolar)
-convertBoton.addEventListener('click', dolartolocal)
+    if (monedaLocal && monedaLocal > 0) {
+        inputDolar.value = monedaLocal / 500;
+    }
+    if (dolar && dolar > 0) {
+        inputLocal.value = dolar * 500;
+    }
+} //Los if toman de condicion por si el usuario intenta o no ingresar ningun valor o ingresa valores negativos que no son validos
+convertBoton.addEventListener('click', convert)
 deleteBoton.addEventListener('click', clearInputs)
+*/
